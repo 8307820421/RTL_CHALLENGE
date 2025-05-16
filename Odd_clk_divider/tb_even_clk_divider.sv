123fe9d10e923306f8816cbfbb812089405f4446
Sonu Tripathi <110840084+8307820421@users.noreply.github.com>
2025-05-16 12:07:28 +0530

    Update tb_even_clk_divider.sv

--- Odd_clk_divider/tb_even_clk_divider.sv
+++ Odd_clk_divider/tb_even_clk_divider.sv
@@ -22,7 +22,7 @@ end
 
 initial begin
         // Initialize reset
-        $dumpfile("fractional_clock_divider.vcd");
+ $dumpfile("odd_clock_divider.vcd");
  $dumpvars(0, tb_odd_clk_divider);
         
     odd_reset = 1'b1; // assert
